8e1d78ef8b3aad6bd03b4e96158494c47725cf3a
Decucin <64794604+Decucin@users.noreply.github.com>
2021-04-04 21:41:25 +0800

    Add files via upload
    对部分代码进行了改进优化

--- 复数运算/main.cpp
+++ 复数运算/main.cpp
@@ -1,31 +1,23 @@
 #include "cpxNum.h"
 
 int main() {
-	double c1_ral, c1_imag, c2_ral, c2_imag;
-	cpxNum res;
-	char op;
-	cout << "请分别输入两个数的实部和虚部：" << endl;
-	cin >> c1_ral >> c1_imag >> c2_ral >> c2_imag;
-	cpxNum c1(c1_ral, c1_imag);
-	cpxNum c2(c2_ral, c2_imag);
-	do{
-		cout << "请选择要对两个数进行的操作：（+ - * /）" << endl;
-		cin >> op;
-		if (op == '+')
-			res = c1 + c2;
-		else if (op == '-')
-			res = c1 - c2;
-		else if (op == '*')
-			res = c1 * c2;
-		else if (op == '/')
-			res = c1 / c2;
-		else
-			cout << "输入错误，请重新输入！" << endl;
-	} while (op != '+' && op != '-' && op != '*' && op != '/');
+	double real, imag;
+	cout << "请输入第一个复数的实部和虚部: ";
+	cin >> real >> imag;
+	cpxNum c1(real, imag); //初始化‘复数’c1
+	cout << "您生成的第一个复数是: ";
 	c1.print();
-	cout << " "<<op<<" ";
+	cout << endl << "请输入第二个复数的实部和虚部: ";
+	cin >> real >> imag;
+	cpxNum c2(real, imag); //初始化‘复数’c2
+	cout << "您生成的第二个复数是: ";
 	c2.print();
-	cout << " = ";
-	res.print();
+	cout << endl;
+	cout << "***********复数运算测试*************" << endl;
+	cout << "c1 + c2的结果是: "; (c1 + c2).print(); cout << endl;
+	cout << "c1 - c2的结果是: "; (c1 - c2).print(); cout << endl;
+	cout << "c1 * c2的结果是: "; (c1 * c2).print(); cout << endl;
+	cout << "c1 / c2的结果是: "; (c1 / c2).print(); cout << endl;
+	cout << "***********复数运算测试结束*************" << endl;
 	return 0;
 }
